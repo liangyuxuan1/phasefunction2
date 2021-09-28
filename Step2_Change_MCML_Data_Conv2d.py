327b16b5675fdd7c2a5e445234403368284b8359
liangyuxuan1 <872277196@qq.com>
2021-09-28 11:36:34 +0800

    change new data

--- Step2_Change_MCML_Data_Conv2d.py
+++ Step2_Change_MCML_Data_Conv2d.py
@@ -82,7 +82,7 @@ def changeMCML_rawData(data_path, img_path, dataListFile):
 # ==================================================================
 if __name__=='__main__':
 
-    imgSize = 51
+    imgSize = 401
 
     src_path        = f"RawData_MCML_Train_{imgSize}"
     dst_path        = f"ImageCW_Train_{imgSize}"
